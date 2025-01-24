c3ccee9e120ea4f5b44d8f2fd97cab7eba71316c
Felipe Barbosa Carvalho <140024961+FelipeC-05@users.noreply.github.com>
2025-01-24 05:30:15 -0300

    Order, Intant, ISO 8601

---
+++ src/main/java/com/estudandospring/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.estudandospring.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudandospring.course.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
